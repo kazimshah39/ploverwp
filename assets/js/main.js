e8182f922d2d01b9459050c5beea33ef07762f23
kazim <kazimshah39@gmail.com>
2020-10-25 06:35:30 +0500

    fix header height by resize window

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,20 @@
   // Set header margin top according to admin bar
 
   function headerMarginTop() {
-    if ($("#wpadminbar") && $("#wpadminbar").css("display") != "none") {
-      let adminBarHeight = $("#wpadminbar").outerHeight();
+        let adminBarHeight = $("#wpadminbar").outerHeight();
       $("#wpadminbar").css("position", "fixed");
       $("#header").css("top", adminBarHeight);
       $(".mobile-nav-toggle").css("top", adminBarHeight + 15);
       $(".mobile-nav").css("top", adminBarHeight + 50);
-    }
   }
 
-  headerMarginTop();
-
-  $(window).resize(function () {
+  if ($("#wpadminbar") && $("#wpadminbar").css("display") != "none") {
+    
     headerMarginTop();
-  });
+
+    $(window).resize(function () {
+      headerMarginTop();
+    });
+  }
+  
 })(jQuery);
