7159fd92208dc2dfd26f3569daa4f4e5b82afda8
kazim <kazimshah39@gmail.com>
2021-03-04 12:26:56 +0500

    tailwindcss added

--- inc/front-end/enqueue.php
+++ inc/front-end/enqueue.php
@@ -7,9 +7,10 @@ require get_template_directory() . '/inc/customizer/custom-css.php';
 function ploverwp_front_end_scripts() {
 
   wp_enqueue_style('font-awesome', get_theme_file_uri('/assets/css/all.min.css'), [], microtime());
+  wp_enqueue_style('ploverwp-tailwind-css', get_theme_file_uri('/assets/css/dist/tailwind.css'), [], microtime());
+  wp_enqueue_style('ploverwp-main-css', get_theme_file_uri('/assets/css/dist/main.css'), [], microtime());
   wp_enqueue_style('ploverwp-bootstrap-css', get_theme_file_uri('/assets/css/bootstrap.min.css'), [], microtime());
   wp_enqueue_style('ploverwp-sidebar-css', get_theme_file_uri('/assets/css/sidebar.css'), [], microtime());
-  wp_enqueue_style('ploverwp-main-css', get_theme_file_uri('/assets/css/dist/main.css'), [], microtime());
   wp_enqueue_style('ploverwp-style', get_stylesheet_uri(), [], microtime());
   wp_add_inline_style('ploverwp-style', ploverwp_get_customizer_css());
 
